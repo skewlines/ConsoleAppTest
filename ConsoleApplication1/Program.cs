caafba4b66afeb8bfe1b096e17d34d61f135a85e
skewlines <xybule@gmail.com>
2017-07-02 02:23:07 -0500

    Testing new changes

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+
+            for(int i=0;i<a;i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine("Hello! No :" + i);
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
