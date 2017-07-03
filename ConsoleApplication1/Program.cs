6b5ebbe46c56ff0b55fb0a0a8ce47d60edbde412
skewlines <xybule@gmail.com>
2017-07-02 19:30:56 -0500

    Testing local changes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -19,6 +19,8 @@ namespace ConsoleApplication1
                     Console.WriteLine("Hello! No :" + i);
                 }
             }
+
+            TestClass.greet();
             
             Console.ReadLine();
         }
---
+++ ConsoleApplication1/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class TestClass
+    {
+
+        public static void greet()
+        {
+            Console.WriteLine("Hello there");
+        }
+    }
+}
